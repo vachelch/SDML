ce794ea07a5d9d4d2c8705eb595b8c21d8765190
Vachel <r06944043@ntu.edu.tw>
2018-10-15 17:26:55 +0800

    remove data

--- hw1-2/run.sh
+++ hw1-2/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd src
+wget 'https://www.dropbox.com/s/ip1f9gyboylxipa/t2-all.zip?dl=1' -O 't2-all.zip'
+unzip t2-all.zip
 python tf-idf.py
 python gragh_feature.py
 python clf.py
--- hw1-3/run.sh
+++ hw1-3/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd src
+wget 'https://www.dropbox.com/s/iowhfprgzjus27a/t3-all.zip?dl=1' -O 't3-all.zip'
+unzip t3-all.zip
 python tf-idf.py
 python time.py
 python gragh_feature.py
